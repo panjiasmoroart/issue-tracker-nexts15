edbf3bb43d5805520b64b3007bbbeb772a8428b9
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-10-24 00:07:44 +0700

    Creating Issues - Building the New Issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@radix-ui/themes";
-
 export default function Home() {
-  return (
-    <div>
-      <Button>New Issue</Button>
-    </div>
-  );
+  return <div>Halaman Dashboard</div>;
 }
