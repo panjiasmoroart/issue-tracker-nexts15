be5b309dc3fc808a4ddb65452f889bd8f74142c2
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-10-23 23:41:28 +0700

    Creating Issues - Building an API

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from "zod";
+import prisma from '@/prisma/client';
+
+const CreateIssueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = CreateIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
