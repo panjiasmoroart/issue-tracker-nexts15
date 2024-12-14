a946c30c207354454c1f829df717e42babc96dd9
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-14 08:50:47 +0700

    Deleting Issues - Add a Delete Button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 // import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 
@@ -41,7 +41,9 @@ export default function RootLayout({
       > */}
         <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
           {/* <ThemePanel /> */}
         </Theme>
       </body>
