99dd485d50b7c70a607c6d4f33d9e609850800f8
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-21 20:20:31 +0700

    Filter, Sort and Pagination - Building the Filter Component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,14 +1,16 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
-      <Link href="/issues/new">
-        <Button>New Issue</Button>
-      </Link>
-    </div>
+    <Flex mb="5" justify="between">
+      <IssueStatusFilter />
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </Flex>
   );
 };
 
