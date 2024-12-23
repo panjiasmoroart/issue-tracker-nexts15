d975580cb03542768c3e491de72f791a88a4fdc0
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-23 11:38:24 +0700

    Filter, Sort and Pagination - Fix Filtering Bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
   { label: "All" },
@@ -14,18 +14,32 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+        if (status) {
+          params.append("status", status);
+        }
+
+        if (searchParams.get("orderBy")) {
+          params.append("orderBy", searchParams.get("orderBy")!);
+        }
+
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
-        {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value || "all"}>
+        {statuses.map((status, index) => (
+          <Select.Item
+            key={`${status.value}-${index}`}
+            value={status.value || "all"}
+          >
             {status.label}
           </Select.Item>
         ))}
