db15ca5ee2d5d670fd62ec807b6dcff814139f1a
panjiasmoroart <pandjiasmoroart@gmail.com>
2024-12-23 23:17:21 +0700

    Dashboard - Building the IssueSummary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-import Pagination from "./components/Pagination";
-import LatestIssues from "./LatestIssues";
+// import LatestIssues from "./LatestIssues";
 
-export default function Home() {
-  return <LatestIssues />;
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    // <LatestIssues />
+    <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+  );
 }
